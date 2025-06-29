0340cec542e551df25e33d366970679198e2e563
Jasur <j31548359@gmail.com>
2025-06-29 12:00:30 +0500

    o'zgarish2

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const User = require("../models/userModel");
 const Warehouse = require("../models/warehouseModel");
 const Store = require("../models/Store");
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 exports.registerUser = async (req, res) => {
   const { name, login, password, role, success } = req.body;
 
@@ -36,32 +36,37 @@ exports.loginUser = async (req, res) => {
   const { login, password } = req.body;
 
   try {
-    let user
-    let role
-    let success
+    let user;
+    let role;
+    let success;
     user = await User.findOne({ login });
-    const allUsers = await User.find()
+    const allUsers = await User.find();
 
-    role = user?.role
-    success = user?.success
+    role = user?.role;
+    success = user?.success;
     if (!user) {
-      user = await Warehouse.findOne({ login })
-      role = 'warehouse'
-      success = { skaladorlar: true }
+      user = await Warehouse.findOne({ login });
+      role = "warehouse";
+      success = { skaladorlar: true };
     }
     if (!user) {
-      user = await Store.findOne({ login })
-      role = 'store'
-      success = { qarzdorlar: true, xarajatlar: true, skaladorlar: true, dokon: true }
+      user = await Store.findOne({ login });
+      role = "store";
+      success = {
+        qarzdorlar: true,
+        xarajatlar: true,
+        skaladorlar: true,
+        dokon: true,
+      };
     }
 
     if (!user) {
-      return res.status(404).send()
+      return res.status(404).send();
     }
-    const isMatch = await bcrypt.compare(password, user.password)
+    const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(401)
+      return res.status(401);
     }
     return res.status(200).json({
       _id: user._id,
@@ -70,14 +75,12 @@ exports.loginUser = async (req, res) => {
       role: role,
       success: success,
       token: generateToken(user._id, role),
-      hello:"world"
-
-    })
+      hello: "world2",
+    });
   } catch (error) {
     console.log(error);
 
     res.status(500).json({ message: "Server error" });
-
   }
 };
 
